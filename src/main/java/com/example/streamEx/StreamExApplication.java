12f83e73242f990ecc8afff9dabdeebe3a9f5182
HeeyeonKim18 <go1927hm@gmail.com>
2024-10-04 15:56:15 +0900

    first commit

---
+++ src/main/java/com/example/streamEx/StreamExApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/streamEx/StreamExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
